af7313586eb05cfc7efb1bfd4889a4d9b5c51f97
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-04-29 20:58:16 -0300

    :recycle: refactor: reestrutura connection.ts

--- backend/src/connection.ts
+++ backend/src/connection.ts
@@ -1,11 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/publicGallery';
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/publicGallery';
+const connection = (mongoDatabaseURI = 'mongodb://localhost:/model_example') =>
+  mongoose.connect(mongoDatabaseURI);
 
-const connectToDatabase = (
-  mongoDatabaseURI = process.env.MONGO_URI
-    || MONGO_DB_URL,
-) => mongoose.connect(mongoDatabaseURI);
-
-export default connectToDatabase;
+export default connection;
