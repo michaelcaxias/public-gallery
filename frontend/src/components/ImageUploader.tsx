fdf15d2aeca1a972326d86c1825be74451dceca1
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-04-29 16:02:57 -0300

    :sparkles: feat: altera ícone de input tipo file

--- frontend/src/components/ImageUploader.tsx
+++ frontend/src/components/ImageUploader.tsx
@@ -1,3 +1,5 @@
+import { BsFillImageFill } from 'react-icons/bs';
+
 export default function ImageUploader() {
   return (
     <div className="grid grid-cols-1 mt-5 mx-7">
@@ -12,7 +14,7 @@ export default function ImageUploader() {
           className="flex flex-col border-4 border-dashed w-full h-32 hover:bg-gray-100 hover:border-purple-300 group"
         >
           <div className="flex flex-col items-center justify-center pt-7">
-            <svg className="w-10 h-10 text-purple-400 group-hover:text-purple-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" /></svg>
+            <BsFillImageFill className="w-10 h-10 text-purple-400 group-hover:text-purple-600" />
             <p
               className="lowercase text-sm text-gray-400 group-hover:text-purple-600 pt-1 tracking-wider"
             >
