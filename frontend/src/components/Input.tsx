503a0fa25605491618826c80323b334cd5d6c6d3
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-04-29 15:38:17 -0300

    :label: cria tipagem para title em componente input

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -1,17 +1,21 @@
-export default function Button() {
+type InputProps = {
+  title: string,
+}
+
+export default function Button({ title }: InputProps) {
   return (
     <div className="grid grid-cols-1 mt-5 mx-7">
       <label
         htmlFor="input1"
         className="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold"
       >
-        Input 1
+        {title}
       </label>
       <input
         id="input1"
         className="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
         type="text"
-        placeholder="Input 1"
+        placeholder={title}
       />
     </div>
   );
