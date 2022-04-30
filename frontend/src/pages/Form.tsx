04df241b5123f5746994f1126c8f5dd168e58379
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-04-30 13:57:53 -0300

    :sparkles: style: ajusta formatação dos elementos

--- frontend/src/pages/Form.tsx
+++ frontend/src/pages/Form.tsx
@@ -56,14 +56,37 @@ export default function Form() {
 
   return (
     <div className="flex h-screen bg-gray-200 items-center justify-center">
-      <form onSubmit={handleFormSubmit} className="grid bg-white rounded-lg py-5 shadow-xl w-11/12 md:w-9/12 lg:w-1/2 border-purple-700">
+      <form
+        onSubmit={handleFormSubmit}
+        className="grid bg-white rounded-lg py-5 shadow-xl w-11/12 md:w-9/12 lg:w-1/2 border-purple-700"
+      >
         <h1 className="text-gray-600 font-bold md:text-2xl text-xl text-center">Public Gallery</h1>
-        <Input title="Título da Imagem" id="title" inputRef={titleRef} />
-        <Input title="Seu nome" id="name" inputRef={nameRef} />
+        <Input
+          title="Título da Imagem"
+          id="title"
+          inputRef={titleRef}
+        />
+        <Input
+          title="Seu nome"
+          id="name"
+          inputRef={nameRef}
+        />
         <ImageUploader imageRef={imagRef} />
         <div className="flex items-center justify-center  md:gap-4 gap-2 pt-5 pb-5">
-          <Button type="button" className="bg-gray-500 hover:bg-gray-700 text-white">Cancelar</Button>
-          <Button type="submit" className="bg-purple-500 hover:bg-purple-700 text-white">Publicar</Button>
+          <Button
+            type="button"
+            className="bg-gray-500 hover:bg-gray-700 text-white"
+          >
+            Cancelar
+
+          </Button>
+          <Button
+            type="submit"
+            className="bg-purple-500 hover:bg-purple-700 text-white"
+          >
+            Publicar
+
+          </Button>
         </div>
       </form>
     </div>
