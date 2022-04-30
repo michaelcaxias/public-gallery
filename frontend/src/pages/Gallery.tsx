eb56bff0b33fd8c52a5135a73a10ce565623c504
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-04-30 15:08:03 -0300

    :sparkles: feat: gallery - implementa requisição de todas as imagens

--- frontend/src/pages/Gallery.tsx
+++ frontend/src/pages/Gallery.tsx
@@ -1,11 +1,23 @@
+import { useEffect, useState } from 'react';
 import ImageCard from '../components/ImageCard';
 
 export default function Gallery() {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const getImages = async () => {
+      const response = await fetch('http://0.0.0.0:3001/images');
+      const json = await response.json();
+      setImages(json);
+    };
+    getImages();
+  }, []);
+
   return (
     <div className="flex flex-col h-screen bg-gray-200 items-center justify-center p-3 gap-4">
       <h1 className="text-gray-600 font-bold md:text-2xl text-xl text-center">Gallery</h1>
-      <div className="grid bg-white rounded-lg py-5 shadow-xl h-full w-full overflow-y-auto">
-        <ImageCard />
+      <div className="grid grid-cols-[repeat(8,_1fr)] grid-rows-[repeat(8,_5vw)] gap-2 bg-white rounded-lg py-5 px-3 shadow-xl h-full w-full overflow-y-auto">
+        {images && images.map((data) => (<ImageCard id={data._id} src={data.image} alt={data.title} />))}
       </div>
     </div>
   );
