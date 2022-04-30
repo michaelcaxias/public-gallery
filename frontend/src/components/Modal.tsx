fd984a87e2d364a5592c073809fcf737b25da93d
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-04-30 20:27:57 -0300

    :sparkles: feat: cria implementação para fechar modal ao clicar no fundo

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -1,12 +1,29 @@
-import { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { FormContext } from '../context/Provider';
 
 export default function Modal() {
-  const { handleClose, modalData } = useContext(FormContext);
-  const { title, image, author} = modalData;
-  const [isMaximized, maximize] = useState(false);
+  const { modalData, handleClose } = useContext(FormContext);
+
+  const closeModal = (
+    event: React.MouseEvent<HTMLDivElement,
+    globalThis.MouseEvent> & { target: HTMLDivElement },
+  ) => {
+    if (event.target.className.includes('modal')) {
+      handleClose();
+    }
+  };
 
   return (
-    <div>Modal</div>
+    <div
+      onClick={(event) => closeModal(
+        event as React.MouseEvent<HTMLDivElement,
+        globalThis.MouseEvent> & { target: HTMLDivElement },
+      )}
+      role="button"
+      tabIndex={0}
+      className="modal flex items-center justify-center absolute z-10 w-full h-full backdrop-blur-sm"
+    >
+      <img className="max-w-[90%] max-h-[90%]" src={modalData.image} alt="imgur" />
+    </div>
   );
 }
