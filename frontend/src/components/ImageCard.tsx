4240816b115383d33ce55769b5cd6c633c7c862d
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-04-30 18:43:12 -0300

    label: style: imagecard - altera de alt para title

--- frontend/src/components/ImageCard.tsx
+++ frontend/src/components/ImageCard.tsx
@@ -1,13 +1,14 @@
 type ImageCardProps = {
   src: string,
-  alt: string,
+  title: string,
   id: string,
 }
 
-export default function ImageCard({ src, alt, id }: ImageCardProps) {
+export default function ImageCard({ src, title, id }: ImageCardProps) {
   return (
-    <div className="cursor-pointer inline-block">
-      <img id={id} className="rounded shadow-xl" src={src} alt={alt} draggable={false} />
+    <div className="flex flex-col cursor-pointer items-center bg-purple-200 py-2 rounded-lg">
+      <img id={id} className="rounded shadow-xl" src={src} alt={title} draggable={false} />
+      <p>{title}</p>
     </div>
   );
 }
