263682b62c542ad2ec943e0037ff91683cdeb909
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-04-30 19:45:52 -0300

    :sparkles: feat: cria implementação para uso de modal

--- frontend/src/context/Provider.tsx
+++ frontend/src/context/Provider.tsx
@@ -1,24 +1,39 @@
-/* eslint-disable no-unused-vars */
 import React, { createContext, useState, useMemo } from 'react';
+import { ImageAPI } from '../pages/Gallery';
 
 type FormContextProps = {
   children: React.ReactNode,
 }
 
 export type TContext = {
-  image: string,
-  setImage: (image: string) => void,
+  isOpen: boolean,
+  handleOpen: () => void,
+  handleClose: () => void,
+  modalData: ImageAPI,
+  setModalData: React.Dispatch<React.SetStateAction<ImageAPI>>,
 }
 
 export const FormContext = createContext({} as TContext);
 
 export function Provider({ children }: FormContextProps) {
-  const [image, setImage] = useState('');
+  const [modalData, setModalData] = useState<ImageAPI>({} as ImageAPI);
+  const [isOpen, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   const context = useMemo(() => ({
-    image,
-    setImage,
-  }), [image]);
+    isOpen,
+    handleOpen,
+    handleClose,
+    modalData,
+    setModalData,
+  }), []);
 
   return (
     <FormContext.Provider value={context}>
