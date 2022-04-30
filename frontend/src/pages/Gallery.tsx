df83658b95f2d60cab2d67243659e9ccca39b769
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-04-30 14:13:03 -0300

    :sparkles: feat: cria estrutura básica de css de gallery

--- frontend/src/pages/Gallery.tsx
+++ frontend/src/pages/Gallery.tsx
@@ -1,7 +1,10 @@
 export default function Gallery() {
   return (
-    <div>
-      <h1 className="text-center">Gallery</h1>
+    <div className="flex flex-col h-screen bg-gray-200 items-center justify-center p-3 gap-4">
+      <h1 className="text-gray-600 font-bold md:text-2xl text-xl text-center">Gallery</h1>
+      <div className="grid bg-white rounded-lg py-5 shadow-xl h-full w-full overflow-y-auto">
+        Cards
+      </div>
     </div>
   );
 }
