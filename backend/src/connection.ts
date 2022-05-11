11255ad62b75be2bdeef9029ced9c07731760288
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-05-10 21:46:28 -0300

    :boom: fix: corrige erro ao subir backend

--- backend/src/connection.ts
+++ backend/src/connection.ts
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
 
+const MONGO_URL = 'mongodb://mongo:27017/public-gallery';
+
 const connection = (
-  mongoDatabaseURI = 'mongodb://localhost:27017/public_gallery',
+  mongoDatabaseURI = MONGO_URL,
 ) =>
   mongoose.connect(mongoDatabaseURI);
 
